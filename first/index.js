7662cc291dde9863d8c3686c7c644807400afcc4
AKuchevskaya <alena.kuchevskaya@yandex.ru>
2023-02-05 13:11:12 +0300

    добавить определние hideBlockButton

--- first/index.js
+++ first/index.js
@@ -3,6 +3,8 @@ myModal.toggle();
 
 const headerBlock = document.querySelector('.container__header');
 const redBlock = document.querySelector('.container__main_clr-red');
+const hideBlockButton = document.querySelector('.btn-warning');
+const changeBlocksButton = document.querySelector('.btn-success');
 
 hideBlockButton.addEventListener('click', () => {
     headerBlock.classList.toggle('d-none');
