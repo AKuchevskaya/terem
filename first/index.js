f84699e87105c89aacdf47c4d8f30a58449bc616
AKuchevskaya <alena.kuchevskaya@yandex.ru>
2023-02-05 13:03:32 +0300

    настроить кнопку переключения блоков и удалить лишние стили

--- first/index.js
+++ first/index.js
@@ -1,44 +1,13 @@
-
 const myModal = new bootstrap.Modal(document.getElementById('myModal'), {})
 myModal.toggle();
 
 const headerBlock = document.querySelector('.container__header');
-const blueBlock = document.querySelector('.container__main_clr-blue');
 const redBlock = document.querySelector('.container__main_clr-red');
-const hideBlockButton = document.querySelector('.btn-warning');
-const changeBlocksButton = document.querySelector('.btn-success');
 
-const hideBlock = () => {
-    headerBlock.style.display = 'none'
-}
-const showBlock = () => {
-    headerBlock.style.display = 'block'
-}
+hideBlockButton.addEventListener('click', () => {
+    headerBlock.classList.toggle('d-none');
+});
 
-const changeVisibilityBlock = () => {
-    if (headerBlock.style.display == 'none') {
-        showBlock();
-    } else {
-        hideBlock();
-    }
-}
-
-hideBlockButton.addEventListener('click', changeVisibilityBlock);
-
-const moveRedBlock = () => {
-    redBlock.style.order = '-1'
-    blueBlock.style.order = '1'
-}
-const backRedBlock = () => {
-    redBlock.style.order = '1'
-    blueBlock.style.order = '-1'
-}
-const changeBlocks = () => {
-    if (redBlock.style.order == '-1' && blueBlock.style.order == '1') {
-        backRedBlock();
-    } else {
-        moveRedBlock();
-    }
-}
- 
-changeBlocksButton.addEventListener('click', changeBlocks);
+changeBlocksButton.addEventListener('click', () => {
+    redBlock.classList.toggle('order-first');
+});
