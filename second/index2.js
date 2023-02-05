cf57ebc5ae8fe8e8f700767130f9ab29ba78a066
AKuchevskaya <alena.kuchevskaya@yandex.ru>
2023-02-05 13:18:22 +0300

    дополнить стэк в README удалить лишние файлы

--- second/index2.js
+++ second/index2.js
@@ -1,34 +1,31 @@
-
-
-
 const { form } = document.forms;
 
 function getInputs(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const formData = new FormData(form);
-    const values = JSON.stringify(Object.fromEntries(formData.entries()));
-    
-    const answers = document.createElement('div');
-    answers.className = 'answers';
-    answers.innerHTML = `<p>${values}</p>`;
-    document.body.append(answers);
+  const formData = new FormData(form);
+  const values = JSON.stringify(Object.fromEntries(formData.entries()));
+
+  const answers = document.createElement("div");
+  answers.className = "answers";
+  answers.innerHTML = `<p>${values}</p>`;
+  document.body.append(answers);
 }
 
-form.addEventListener('submit', getInputs);
+form.addEventListener("submit", getInputs);
 
-
-    fetch('package.json', {
-        method: 'GET'
-    })
-    .then((res) => {
-       if (res.ok) {
-        return res.json()
-       } throw new Error();
-    })
-    .then((data)=>{
-        alert("данные получены");
-    })
-    .catch((err)=>{
-        console.log('ошибка', err);
-    })
+fetch("package.json", {
+  method: "GET",
+})
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw new Error();
+  })
+  .then((data) => {
+    alert("данные получены");
+  })
+  .catch((err) => {
+    console.log("ошибка", err);
+  });
